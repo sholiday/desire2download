40e16f02936897feedd5f720ead0b6e5596dcd59
Ansis Brammanis <ansis.brammanis@gmail.com>
2012-03-02 23:10:29 -0500

    Some cleanup. Closer to PEP8 and unused code removed.

--- desire2download.py
+++ desire2download.py
@@ -10,85 +10,76 @@ Copyright (c) 2011 Stephen Holiday. All rights reserved.
 import re
 import os
 import urlparse
-from urllib import urlencode
 import mechanize
 import BeautifulSoup
 
 import sys
-reload(sys) 
+reload(sys)
 sys.setdefaultencoding("utf-8")
+
 def retry(howmany):
-    '''
+    """
         Nifty decorator stolen from http://stackoverflow.com/a/567697/429688
-    '''
+    """
     def tryIt(func, *args, **kwargs):
         def f(*args, **kwargs):
             attempts = 0
             while attempts < howmany:
                 try:
                     return func(*args, **kwargs)
-                except Exception, e:
-                    print 'Exception [%s], retrying'%e
+                except Exception as e:
+                    print 'Exception [%s], retrying' % e
                     attempts += 1
         return f
     return tryIt
+
+
 class Desire2Download(object):
     base_url = 'https://learn.uwaterloo.ca/d2l/lp/homepage/home.d2l?ou=6606'
     cas_login = 'https://cas.uwaterloo.ca/cas/login?service=http%3a%2f%2flearn.uwaterloo.ca%2fd2l%2forgtools%2fCAS%2fDefault.aspx'
     ping_url = 'http://jobminestats.appspot.com/Ping/ag5zfmpvYm1pbmVzdGF0c3IMCxIFUGl4ZWwYuRcM.gif'
+
     def __init__(self, username, password):
-        self.username=username
-        self.password=password
-        
+        self.username = username
+        self.password = password
+
         self.br = mechanize.Browser(factory=mechanize.RobustFactory())
         self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-        #self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071635 Fedora/4.0.1-1.fc9 Firefox/4.0.1')]
-        
+ 
         self.br.open(self.ping_url).read()
-        
-    def safe_unicode(self,obj):
-        try:
-            return str(obj)
-        except UnicodeEncodeError:
-            # obj is unicode
-            return unicode(obj).encode('utf-8')
-    
+ 
     @retry(3)
     def login(self):
         print 'Logging In...'
         
-        #self.br.open("https://learn.uwaterloo.ca/")
         self.br.open(self.cas_login)
         
         self.br.select_form(nr=0)
-        self.br['username']=self.username
-        self.br['password']=self.password
+        self.br['username'] = self.username
+        self.br['password'] = self.password
         response = self.br.submit().read()
         print 'Logged In'
         
-        
-    
-            
     def get_course_links(self):
         print 'Finding courses...'
-        links=list()
+        links = []
         for link in self.br.links():
-            matches=re.match('[A-Z]+ [0-9]{3} - [A-Z][a-z]+ 20[0-9]{2}', link.text)
+            matches = re.match('[A-Z]+ [0-9]{3} - [A-Z][a-z]+ 20[0-9]{2}', link.text)
             if matches is not None:
                 links.append(link)
         return links
         
-    def _nice_regex(self,regex,content,group):
-        res=re.search(regex,content)
-        if res!=None:
+    def _nice_regex(self, regex, content, group):
+        res = re.search(regex, content)
+        if res != None:
             return res.group(group)
         else:
             return ''
     
     def convert_bytes(self, bytes):
-        '''
+        """
             Stolen from http://www.5dollarwhitebox.org/drupal/node/84
-        '''
+        """
         bytes = float(bytes)
         if bytes >= 1099511627776:
             terabytes = bytes / 1099511627776
@@ -109,87 +100,79 @@ class Desire2Download(object):
     @retry(3)
     def get_course_documents(self, link):
         self.br.follow_link(link)
-        content_link=None
+        content_link = None
         for link_j in self.br.links():
-            
             if link_j.text == 'Content':
                 if content_link is None:
-                    content_link=link_j
+                    content_link = link_j
                     
         self.br.follow_link(content_link)
         
-        print_dl_link=None
+        print_dl_link = None
         for link_k in self.br.links(url_regex='print_download.d2l'):
             if print_dl_link is not None:
                 print_dl_link = link_k
         
         r = self.br.follow_link(print_dl_link)
         
-        
         page = r.read()
-        #print page
         soup = BeautifulSoup.BeautifulSoup(page)
         table = soup.find(id='z_n')
         
-        document_tree={}
-        path_to_root = list()
+        document_tree = {}
+        path_to_root = []
         
-        rows=table.findAll('tr')
+        rows = table.findAll('tr')
         for row in rows[1:]:
             columns = row.findAll('td')
             
-            depth = len(columns)-2
+            depth = len(columns) - 2
             
             cell = None
             for column in columns:
                 if column.has_key('class') and column['class'] == 'd_gn':
                     cell = column
                    
-            cell_str = ''.join(map(lambda x: x.__str__(), cell.contents ))
+            cell_str = ''.join(map(lambda x: str(x), cell.contents))
             
-            is_heading = True
             if re.search('href=', cell_str):
-                is_heading = False
+                ## Isn't heading
                 link = cell.a
                 if hasattr(link, 'img'):
                     link.img.extract()
                 
-                title = ''.join(map(lambda x: x.__str__(), link.contents ))
+                title = ''.join(map(lambda x: str(x), link.contents))
 
                 ou = self._nice_regex('\?ou\=([0-9]+)', link['href'], 1)
                 tId = self._nice_regex('\&tId\=([0-9]+)', link['href'], 1)
                 
-                link_href = 'https://learn.uwaterloo.ca/d2l/lms/content/preview.d2l?tId=%s&ou=%s'%(tId, ou)
-                
+                link_href = 'https://learn.uwaterloo.ca/d2l/lms/content/preview.d2l?tId=%s&ou=%s' % (tId, ou)
                 
                 cur_tree_node = document_tree
                 for cur_path_node in path_to_root:
                     key = cur_path_node['title']
                     if not cur_tree_node.has_key(key):
-                        cur_tree_node[key]=dict()
-                    cur_tree_node=cur_tree_node[key]
+                        cur_tree_node[key] = {}
+                    cur_tree_node = cur_tree_node[key]
                     
-                cur_tree_node[title]=link_href
+                cur_tree_node[title] = link_href
                 
             else:
-                cell_str = cell_str.replace('&nbsp;','').strip()
-                cell_str = cell_str.replace('<strong>','').replace('</strong>','').strip()
-                node = {'heading':True, 'title':cell_str}
-                    
+                cell_str = cell_str.replace('&nbsp;', '').strip()
+                cell_str = cell_str.replace('<strong>', '').replace('</strong>', '').strip()
+                node = {'heading': True, 'title': cell_str}
             
                 if len(path_to_root) < depth:
                     path_to_root.append(node)
                 else:
-                    path_to_root=path_to_root[:depth]
+                    path_to_root = path_to_root[:depth]
                     path_to_root.append(node)
-            
             
         return document_tree
             
-    def download_tree(self, root, _path=list()):
+    def download_tree(self, root, _path=[]):
         for k in root:
-            path=_path[:]
-            
+            path = _path[:]
             node = root[k]
             
             if type(node) is dict:
@@ -198,45 +181,40 @@ class Desire2Download(object):
             else:
                 title = k
                 url = node
-                path = '/'.join(map(lambda x: x.replace('/','\/'), path))
+                path = '/'.join(map(lambda x: x.replace('/', '\/'), path))
                 
                 try:
                     os.makedirs(path)
                 except:
                     pass
                     
-                #print url
                 page = self.br.open(url).read()
                 soup = BeautifulSoup.BeautifulSoup(page)
                 url = soup.find('iframe')['src']
                 url_path = url.split('?')[0]
                 split = urlparse.urlsplit(url_path)
                 if split.netloc == '':
-                    url = 'https://learn.uwaterloo.ca%s'%url_path
+                    url = 'https://learn.uwaterloo.ca%s' % url_path
                 else:
                     url = url_path
                     url_path = split.path
                     
-                clean_url =  url.replace(' ', '%20')
+                clean_url = url.replace(' ', '%20')
                 
-                if 'https://learn.uwaterloo.ca/d2l/common/dialogs/' in url:
-                    pass
-                
-                else:
-                    
+                if 'https://learn.uwaterloo.ca/d2l/common/dialogs/' not in url:
                     file_name = os.path.split(url_path)[1]
                     
                     skip = False
                     for r in self.ignore_re:
                         if r.match(file_name) is not None:
-                            print 'Skipping %s because it matches ignore regex "%s"'%(file_name, r.pattern)
+                            print 'Skipping %s because it matches ignore regex "%s"' % (file_name, r.pattern)
                             skip = True
                     
                     if not skip:
-                        path_and_filename = '%s/%s'%(path,file_name.strip('/'))
+                        path_and_filename = '%s/%s' % (path, file_name.strip('/'))
                     
                         if os.path.isfile(path_and_filename):
-                            print ' - %s (Already Saved)'%path_and_filename
+                            print ' - %s (Already Saved)' % path_and_filename
                         else:
                             content = self.br.open_novisit(clean_url).read()
                         
@@ -244,4 +222,4 @@ class Desire2Download(object):
                             f.write(content)
                             f.close()
                         
-                            print ' + %s (%s)'%(path_and_filename, self.convert_bytes(len(content)))
+                            print ' + %s (%s)' % (path_and_filename, self.convert_bytes(len(content)))
