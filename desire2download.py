312ed50ac7c7b2fc19b9ed50304337ba8e062627
Stephen Holiday <stephen.holiday@gmail.com>
2012-07-25 21:06:59 -0400

    Fixed regex as per @KartikTalwar's gist: https://gist.github.com/2899403/5e15e4fb4d874607c0c429d0f8071a793df9d2d5

--- desire2download.py
+++ desire2download.py
@@ -81,7 +81,7 @@ class Desire2Download(object):
         links = []
         urls = []
         for link in self.br.links():
-            matches = re.match('[A-Z]+ [0-9A-Z]{2,4} - [A-Z][a-z]+ 20[0-9]{2}', link.text)
+            matches = re.match('[A-Z]+ [0-9A-Z/\s]{2,15} - [A-Z][a-z]+ 20[0-9]{2}', link.text)
             if matches is not None and link.url not in urls:
                 links.append(link)
                 urls.append(link.url)
