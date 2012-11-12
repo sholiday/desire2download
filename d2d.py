d91b96db2d895c60b7906482c036baeac7555654
parryjacob <parry.jacob@gmail.com>
2012-11-11 23:24:08 -0500

    added ability to ignore courses

--- d2d.py
+++ d2d.py
@@ -102,6 +102,7 @@ Other Options:
     -u, --username [username]   set your username
     -p, --password [password]   set your password
     -i, --ignore  [regular exp] ignore files that match this regex
+    -c, --courses [regular exp] ignore courses that match this regex
 '''
             
 class Usage(Exception):
@@ -114,13 +115,14 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "hupi:v", ["help", "username=", "password=","ignore="])
+            opts, args = getopt.getopt(argv[1:], "hupi:c:v", ["help", "username=", "password=","ignore=","courses="])
         except getopt.error, msg:
             raise Usage(msg)
             
         username = None
         password = None
         ignore_re = list()
+        ignore_course = list()
         
         # option processing
         for option, value in opts:
@@ -135,6 +137,12 @@ def main(argv=None):
             if option in ("-i", "--ignore"):
                 try:
                     ignore_re.append(re.compile(value))
+                except:
+                    print 'Regular Expression "%s" is invalid...'
+                    raise Usage(help_message)
+            if option in ("-c", "--courses"):
+                try:
+                    ignore_course.append(re.compile(value))
                 except:
                     print 'Regular Expression "%s" is invalid...'
                     raise Usage(help_message)
@@ -154,13 +162,20 @@ def main(argv=None):
             return 2
         links = d2d.get_course_links()
         for link in links:
-            print link.text
-            try:
-                document_tree = d2d.get_course_documents(link, link.text)
-            except Exception as e: ## TODO: replace Exception
-                print e
-                return 2
-            d2d.download_tree(document_tree)
+            isSkip = False # Use a flag to avoid nasty loop-breaking
+            for r in ignore_course:
+                if r.match(link.text) is not None:
+                    print 'Skipping %s because it matches regex "%s"' % (link.text, r.pattern)
+                    isSkip = True
+            
+            if not isSkip:
+                print link.text
+                try:
+                    document_tree = d2d.get_course_documents(link, link.text)
+                    d2d.download_tree(document_tree)
+                except Exception as e: ## TODO: replace Exception
+                    print 'Failed to load course:', e
+                    #return 2 #don't want to fail just because of one course
         
     except Usage, err:
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
